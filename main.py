f6095c943448a22d46d3e7b73b8972fb173df0dc
feng wang <48723262+slzyslzy@users.noreply.github.com>
2021-09-06 02:31:57 +0800

    初稿完成

--- main.py
+++ main.py
@@ -4,13 +4,13 @@
 @Author  : WangFeng
 """
 import random
+import re
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 import time
 import random
-
 
 # 要爬取的数据：
 # 基础属性
@@ -56,41 +56,157 @@ HouseTradeDataListPrise = []
 # 成交价
 HouseTradeTransactionPrice = []
 
-
-
+pattern = re.compile(r'[0-9]+')  # 查找数字
 base_url = 'https://su.lianjia.com/chengjiao/'
 page_url = []
 page_url.append(base_url)
+
+zh2num = {
+    '零': 0,
+    '一': 1,
+    '二': 2,
+    '两': 2,
+    '三': 3,
+    '四': 4,
+    '五': 5,
+    '六': 6,
+    '七': 7,
+    '八': 8,
+    '九': 9,
+    '十': 10,
+}
+
 for i in range(2, 101):
     page_url.append(base_url + 'pg' + str(i))
 
 
 def get_primary_data(primary_page_url):
     html = requests.get(primary_page_url).text
-    BeautifulSoup(html, "lxml")
     sp = BeautifulSoup(html, "lxml")
     item_list = sp.body.find_all(class_='listContent')[0].contents
     for j in range(0, len(item_list)):
+        # 房屋面积 几室几厅
         title = item_list[j].div.find_all(class_='title')[0].a.text
+        if len(title.split(' ')) != 3:
+            print('非房子')
+            continue
         house_name = title.split(' ')[0]
         house_struct = title.split(' ')[1]
         house_square = title.split(' ')[2]
+        print(house_name, house_square, house_struct)
+
+        # 房屋朝向 房屋装修
+        info = item_list[j].div.find_all(class_='houseInfo')[0].text
+        print(info)
+        list_a = info.replace(' ', '').split('|')
+        house_orientation = list_a[0]
+        house_renovation = list_a[1]
+        print(house_orientation, house_renovation)
+
+        # 成交日期
+        deal_date = item_list[j].div.find_all(class_='dealDate')[0].text
+        print(deal_date)
+
+        # 成交价
+        total_price = item_list[j].div.find_all(class_='totalPrice')[0].text
+        print(total_price)
+
+        # 挂牌价 成交周期
+        deal_info = item_list[j].div.find_all(class_='dealCycleTxt')[0].text
+        # print(deal_info)
+        result = pattern.findall(deal_info)
+        list_price = result[0]
+        trade_cycle = result[1]
+        print(list_price, trade_cycle)
 
         detail_page_url = item_list[j].div.find_all(class_='title')[0].a['href']
+        get_detail_data(detail_page_url)
         # todo get_detail_data()
 
 
-    sp.body.find_all(class_='listContent')[0].contents[0].div.find_all(class_='title')[0].a['href']
+def get_detail_data(detail_page_url):
+    html_1 = requests.get(detail_page_url).text
+    sp = BeautifulSoup(html_1, "lxml")
+    house_follow = sp.body.find_all(class_='msg')[0].contents[4].label.text
+    base_info_contents = sp.body.find_all(class_='content')[0].ul.contents
+    for item in base_info_contents:
+        base_info_label = item.span.text
+        base_info_value = item.text.replace(' ', '').replace(base_info_label, '')
+        print(base_info_label, ':', base_info_value)
+        if base_info_label == '房屋户型':
+            re_res = re.findall(r'[0-9]室', base_info_value)
+            if len(re_res) != 0:
+                bedroom_num = re_res[0][0]
+            else:
+                bedroom_num = None
+            re_res = re.findall(r'[0-9]厅', base_info_value)
+            if len(re_res) != 0:
+                livingroom_num = re_res[0][0]
+            else:
+                livingroom_num = None
+            re_res = re.findall(r'[0-9]厨', base_info_value)
+            if len(re_res) != 0:
+                kitchen_num = re_res[0][0]
+            else:
+                kitchen_num = None
+            re_res = re.findall(r'[0-9]卫', base_info_value)
+            if len(re_res) != 0:
+                bathroom_num = re_res[0][0]
+            else:
+                bathroom_num = None
+            print(bedroom_num, livingroom_num, kitchen_num, bathroom_num)
 
+        if base_info_label == '所在楼层':
+            floor = re.findall(r'([中|高|低])楼层', base_info_value)[0]
+            total_floor = re.findall(r'共([0-9]+)层', '中楼层(共23层)')[0]
+            print(floor, total_floor)
 
+        if base_info_label == '建筑面积':
+            square = re.findall(r'([0-9]+(\.[0-9]+)?)㎡', base_info_value)[0][0]
+            print(square)
 
-'https://su.lianjia.com/chengjiao/107104076340.html'
+        if base_info_label == '户型结构':
+            house_struct = base_info_value
+            print(house_struct)
 
+        if base_info_label == '建筑类型':
+            building_type = base_info_value
+            print(building_type)
 
-sp.body.find_all(class_='listContent')[0].contents[0].div.find_all(class_='title')[0].a.text
+        if base_info_label == '房屋朝向':
+            face = base_info_value
+            print(face)
+
+        if base_info_label == '建成年代':
+            built_age = base_info_value
+            print(built_age)
+
+        if base_info_label == '装修情况':
+            condition = base_info_value
+            print(condition)
+
+        if base_info_label == '建筑结构':
+            building_struct = base_info_value
+            print(building_struct)
+
+        if base_info_label == '梯户比例':
+            a = zh2num[re.findall(r'([\w])梯', base_info_value)[0]]
+            b = zh2num[re.findall(r'([\w])户', base_info_value)[0]]
+            if b == 0:
+                ratio = None
+            else:
+                ratio = a/b
+            print(ratio)
+
+        if base_info_label == '配备电梯':
+            has_env = base_info_value
+            print(has_env)
+
+get_primary_data(base_url)
+
 '香堤澜湾 2室2厅 76.11平米'
 
-
+'''
 class HouseSpider():
     def __init__(self):
         # 通过fake_useragent库，随机切换UserAgent,简单伪装爬虫
@@ -163,3 +279,4 @@ class HouseSpider():
 suzhou = HouseSpider()
 suzhou.all_page_url("https://suzhou.newhouse.fang.com/house/s/b9")
 suzhou.data_save()
+'''
